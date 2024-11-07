c1914657e00253067e99b69895e22620e60dde27
Rayane Guemmoud <guemmoudrayane03@gmail.com>
2024-11-07 11:52:46 +0100

    Restructurate product page

--- src/pages/product/product.tsx
+++ src/pages/product/product.tsx
@@ -1,15 +1,28 @@
+import { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import useSWR from 'swr';
+import { Container, Box, Grid2, Stack, Paper, Rating, Typography, Button } from '@mui/material';
 import { fetchKnifeById } from '../../api/api';
-import './product.css';
+
+// Type
+import type { Knife } from '../../type';
+
+// Components
 import Loading from "../../components/Loading/Loading.tsx";
 import Notfound from "../../components/404/404.tsx";
 
+// Styles
+import './product.css';
 
 const ProductPage = () => {
-  const { id } = useParams(); 
+  let { id } = useParams();
+
   const navigate = useNavigate();
-  const { data: knife, error } = useSWR(id ? `/knives/${id}` : null, () => id ? fetchKnifeById(id) : null);
+  const [indexImage, setIndexImage] = useState(0);
+
+  id ??= 'not_found';
+
+  const { data: knife, error } = useSWR<Knife>(`/knives/${id}`, () => fetchKnifeById(id));
 
   if (error) return <Notfound />;
   if (!knife) return <Loading />;
@@ -18,33 +31,101 @@ const ProductPage = () => {
     navigate(`/editproduct/${id}`);
   };
 
+  const handleSelectImage = (index: number) => {
+    setIndexImage(index);
+  }
+
   return (
-    <div className="product-page">
-
-      <section className="product-details">
-        <div className="image-section">
-          <img src={knife.image} alt={knife.name} className="knife-image" />
-        </div>
-        <div className="info-section">
-          <h2>{knife.name}</h2>
-          <p className="price">${knife.price.toFixed(2)}</p>
-          <p className="description">{knife.description}</p>
-
-          <div className="attributes">
-            <p><strong>Handle:</strong> {knife.handle}</p>
-            <p><strong>Blade:</strong> {knife.blade}</p>
-            <p><strong>Sharpness:</strong> {knife.sharpness}/10</p>
-            <p><strong>Durability:</strong> {knife.durability}/10</p>
-            <p><strong>Weight:</strong> {knife.weight}g</p>
-            <p><strong>Length:</strong> {knife.length}cm</p>
-          </div>
-        <div className="button-section">
-          <button className="edit-product-button" onClick={handleEditClick}>Edit</button>
-        </div>
-        </div>
-      </section>
-    </div>
-  );
+    <Container maxWidth="xl" sx={{ marginTop: '3rem' }}>
+      <Stack alignItems="center" sx={{ width: '100%' }}>
+        <Grid2 container sx={{ width: '100%' }} justifyContent="center" gap={6}>
+          <Grid2 size={3} justifyItems="flex-end">
+            <Paper elevation={4} sx={{ padding: 2, marginBottom: 2 }}>
+              <img src={knife.images[indexImage]} alt={knife.name} className="knife-image" />
+            </Paper>
+            <Box sx={{ justifySelf: 'flex-start' }}>
+              <Grid2 container direction="row" spacing={2}>
+                {knife.images.map((image, index) => (
+                  <Grid2 size={3}>
+                    <Paper
+                      elevation={4}
+                      sx={{ padding: 1, height: 'fit-content', minWidth: '-webkit-fill-available' }}
+                      onClick={() => handleSelectImage(index)}
+                    >
+                      <img key={index} src={image} alt={knife.name} className="knife-thumbnail" />
+                    </Paper>
+                  </Grid2>
+                ))}
+              </Grid2>
+            </Box>
+          </Grid2>
+          <Grid2 size={3}>
+            <Box>
+              <Typography
+                variant="body1"
+                fontWeight="bold"
+                fontSize={22}
+              >
+                  {knife.name}
+              </Typography>
+              <Typography
+                variant="body1"
+                fontWeight="bold"
+                fontSize={28}
+                sx={{ color: '#ff613a' }}>
+                  ${knife.price.toFixed(2)}
+              </Typography>
+              <Typography
+                variant="body1"
+                fontSize={18}
+                sx={{ color: "#666", marginTop: 2 }}
+              >
+                {knife.description}
+              </Typography>
+              <Stack direction="column" spacing={2} sx={{ marginTop: 2 }}>
+                <p><strong>Handle:</strong> {knife.handle}</p>
+                <p><strong>Blade:</strong> {knife.blade}</p>
+                <p><strong>Weight:</strong> {knife.weight}g</p>
+                <p><strong>Length:</strong> {knife.length} mm</p>
+              </Stack>
+              <Stack flexDirection="row" gap={3}>
+                <Box>
+                  <p><strong>Sharpness:</strong></p>
+                  <Rating
+                    name="sharpness"
+                    value={knife.sharpness / 2}
+                    readOnly
+                  />
+                </Box>
+                <Box>
+                  <p><strong>Durability:</strong></p>
+                  <Rating
+                    name="durability"
+                    value={knife.durability / 2}
+                    readOnly
+                  />
+                </Box>
+              </Stack>
+              <Box>
+                <Button
+                  variant="contained"
+                  sx={{
+                    backgroundColor: '#ff613a',
+                    transition: 'background-color 0.3s ease',
+                    '&:hover': {
+                      backgroundColor: '#ff613a',
+                    },
+                    marginTop: 2,
+                  }}
+                  onClick={handleEditClick}
+                >Edit</Button>
+              </Box>
+            </Box>
+          </Grid2>
+        </Grid2>
+      </Stack>
+    </Container>
+  )
 };
 
 export default ProductPage;
